c7de88c5a6e075fcc2747d26db36347ca5f7e1f9
Dimas Abreu Dutra <dimasadutra@gmail.com>
2016-04-19 18:52:18 -0300

    Implementing AHRS message reading.

--- devices/ahrs400/ahrs400.c
+++ devices/ahrs400/ahrs400.c
@@ -2,9 +2,12 @@
  * Device module for Crossbow's AHRS400 Attitude and Heading Reference System.
  */
 
-
+#include <errno.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
+#include <syslog.h>
 #include <sys/stat.h>
 #include <termios.h>
 #include <unistd.h>
@@ -13,9 +16,8 @@
 /*** AHRS constants ***/
 #define AHRS_DEFAULT_BAUDRATE B38400
 
-#define AHRS_MSG_HEADER 0xFF
-#define AHRS_MSG_LEN 29 //header not included (crc included)
-
+#define AHRS_DATA_HEADER 0xFF
+#define AHRS_MAX_MSG_SIZE 30
 
 /*** AHRS Message codes ***/
 // Communication test messages
@@ -55,66 +57,142 @@
 #define CLEAR_SOFTI_RESPONSE 'T'
 
 
-int open_ahrs(char *file) {
+/**
+ * Open the AHRS serial port stream.
+ * @param The path of the serial port device.
+ * @return The AHRS port stream or NULL if error.
+ */
+FILE* ahrs_open(char *path) {
     int fd = open(file, O_RDWR);
     if (fd < 0) {
-        perror("Error opening AHRS file");
-        return -1;
+        syslog(LOG_ERR, "Error opening AHRS file: %s", strerror(errno));
+        return NULL;
+    }
+    
+    FILE *file = fdopen(fd, "rw");
+    if (file == NULL) {
+        syslog(LOG_ERR, "Error in fdopen: %s", strerror(errno));
+        fclose(fd);
+        return NULL;
     }
     
     struct termios ahrs_termios;
-    if (tcgetattr(fd, &ahrs_termios)) {
-        perror("Error getting AHRS serial port configuration");
+    if (tcgetattr(fd, &ahrs_termios)
+        || cfsetispeed(&ahrs_termios, AHRS_DEFAULT_BAUDRATE)
+        || cfsetospeed(&ahrs_termios, AHRS_DEFAULT_BAUDRATE)
+        || tcsetattr(fd, &ahrs_termios)) {
+        syslog(LOG_WARNING, "Error setting AHRS baud rate: %s",strerror(errno));
+    }
+    
+    return file;
+}
+
+/**
+ * Ping the AHRS.
+ * @param AHRS400 serial port stream.
+ * @return 0 if pong received, -1 if pong not received, if error or if EOF.
+ */
+int ahrs_ping(FILE *file) {
+    if (fputc(PING, file) == EOF) {
+        syslog(LOG_ERROR, "ERROR writing ping to AHRS stream: %s",
+               strerror(errno));
         return -1;
     }
-    if (cfsetispeed(&ahrs_termios, AHRS_DEFAULT_BAUDRATE)) {
-        perror("Error setting  AHRS baud rate");
+
+    int response = fgetc(file);
+    if (response == EOF) {
+        if (feof(file))
+            syslog(LOG_WARNING, "EOF while waiting for ping response");
+        else
+            syslog(LOG_ERROR, "Read error while waiting for ping response: %s",
+                   strerror(file));
         return -1;
     }
-    if (tcsetattr(fd, &ahrs_termios)) {
-        perror("Error setting AHRS serial port configuration");
+    
+    if (response != PING_RESPONSE) {
+        syslog(LOG_INFO, "Invalid ping from AHRS: %#x", response);
         return -1;
     }
-    return fd;
+
+    return 0;
 }
 
 
-int config_ahrs(int fd){
-    // Put system into polled mode for configuration
-    uint8_t data = POLLED_MODE;
-    write(fd, &data, sizeof data);
-    fsync(fd);
-    
-    // Discard all unread data in the buffer
-    tcflush(fd, TCIFLUSH);
+/**
+ * Search for an AHRS header in the stream.
+ * @param AHRS400 serial port stream.
+ * @return 0 if header found, -1 if error or EOF.
+ */
+static int ahrs_search_header(FILE *file) {
+    for (;;) {
+        //Get the next character from the stream
+        int recv = fgetc(file);
+        
+        if (recv == EOF) {
+            if (feof(file))
+                syslog(LOG_WARNING, "EOF while waiting for header");
+            else
+                syslog(LOG_ERROR, "Read error while waiting for header: %s",
+                       strerror(file));
+            return -1;
+        }
 
-    // Ping the device
-    data = PING;
-    write(fd, &data, sizeof data);
-    read(fd, &data, sizeof data);
-    if (data != PING_RESPONSE) {
-        fprintf(stderr, "Invalid response to ping: %c.\n", data);
-        return -1;
+        if (recv == AHRS_DATA_HEADER)
+            return 0;
     }
+}
+
+
+/**
+ * Get a message from the AHRS.
+ * @param AHRS400 serial port stream.
+ * @param packet payload size.
+ * @param[out] pointer to where the payload should be stored.
+ * @return 0 if message read and payload stored, -1 if error or EOF.
+ */
+int ahrs_get_msg(FILE *file, unsigned size, uint8_t *payload) {
+    uint8_t work[size + 1];
+    uint8_t work_ptr = 0;
+    bool header_found = false;
     
-    // Get AHRS version
-    // TODO
+    for (;;) {
+        // Look for header
+        if (header_found) {
+            if (ahrs_search_header(file))
+                return -1;
+        }
 
-    // Put AHRS in angle mode
-    data = ANGLE_MODE;
-    write(fd, &data, sizeof data);
-    read(fd, &data, sizeof data);
-    if (data != ANGLE_MODE_RESPONSE) {
-        fprintf(stderr, "Invalid response to angle mode: %c.\n", data);
-        return -1;
-    }
+        // Get message body and checksum
+        if (!fread(work + work_ptr, sizeof(work) - work_ptr, 1, file)) {
+            if (feof(file))
+                syslog(LOG_WARNING, "EOF while waiting for payload");
+            else
+                syslog(LOG_ERROR, "Read error while waiting for payload: %s",
+                       strerror(file));
+            return -1;
+        }
+        
+        // Calculate checksum
+        uint8_t calculated_chk = 0;
+        for (int i=0; i<size; i++)
+            calculated_chk += work[i];
 
-    // Put AHRS in continuous mode
-    data = CONTINUOUS_MODE;
-    write(fd, &data, sizeof data);
-    read(fd, &data, sizeof data);
-    if (data != CONTINUOUS_MODE_RESPONSE) {
-        fprintf(stderr, "Invalid response to continuous mode: %c.\n", data);
-        return -1;
+        // Check message
+        if (calculated_chk == work[size]) {
+            memcpy(payload, work, size);
+            return 0;
+        }
+
+        // Look for header in work buffer
+        work_ptr = 0;
+        header_found = false;
+        for (int i=0; i<sizeof work; i++) {
+            if (work[i] == AHRS_DATA_HEADER) {
+                memcpy(work, work + i + 1, sizeof(work) - i - 1);
+                work_ptr = sizeof(work) - i - 1;
+                header_found = true;
+                break;
+            }
+        }
     }
 }
